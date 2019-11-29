b204ec934df2849ee22c5a4036cc4a7249b69afd
Nahuel <nahuel.trucco.bkn@gmail.com>
2019-11-29 10:02:22 -0300

    Entrega Final
    Instancia del proyecto en la cual fue entregado a la catedra.
    Luego continuaré trabajando para pulir detalles, con el objetivo de utilizarlo para un portfolio.

--- TDA_Arboles.h
+++ TDA_Arboles.h
@@ -11,10 +11,10 @@
 
 typedef struct {
     int  idCancion;
-    char titulo[30];
-    char artista[30];
+    char titulo[40];
+    char artista[40];
     int  duracion;
-    char album[30];
+    char album[40];
     int  anio;
     char genero[20];
     char comentario[100];
@@ -35,6 +35,6 @@ int fromArchiToArreglo (stCancion arreglo[], int dim);
 nodoArbolCancion * fromArchiToArbol (nodoArbolCancion * arbol);
 
 stCancion inicCancion ();
-stCancion buscarCancionPorID (nodoArbolCancion * raiz, int idCancionBuscada);
+nodoArbolCancion * buscarCancionPorID (nodoArbolCancion * raiz, int idCancionBuscada);
 
 #endif // TDA_ARBOLES_H_INCLUDED
